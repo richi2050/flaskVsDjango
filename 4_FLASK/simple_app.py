27a1353b4133c2e0715afde40d873f5b47edd2a6
Ricardo Lugo Recillas <ricardo.lugo.recillas@gmail.com>
2019-06-11 00:18:09 -0500

    suma de numero con html

--- 4_FLASK/simple_app.py
+++ 4_FLASK/simple_app.py
@@ -4,9 +4,27 @@ app = Flask(__name__)
 
 #Route
 @app.route('/')
-def index(name='Mundo', lastname='!!!!!!!'):
+@app.route('/<name>')
+def index(name='Mundo'):
     name = request.args.get('name', name)
-    lastname = request.args.get('lastname', lastname)
-    return "Holas {} {}".format(name, lastname)
+    return "Holas {}".format(name)
+
+
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+def add(num1, num2):
+    return """
+            <!doctype html>
+            <html>
+            <head>
+                <title>Sumador</title>
+            </head>
+            <body>
+            <h1>{} + {} = {}</h1>
+            </body>
+            </html>
+            """.format(num1, num2, num1 + num2)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
